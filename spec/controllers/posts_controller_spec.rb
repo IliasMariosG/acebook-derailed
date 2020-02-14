8254370406f3a1f2a926864351482b036858b087
Vish Mayer <vish.mayer@gmail.com>
2020-02-14 13:57:11 +0000

    tests for new user and routing added

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe PostsController, type: :controller do
 
   describe "POST /" do
     it "responds with 200" do
-      post :create, params: { post: { message: "Hello, world!" } }
+      post :create, params: { post: { message: "Hello, world!"}, users_id: "1" }
       expect(response).to redirect_to(posts_url)
     end
 
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  describe "GET /new user " do
+    it "responds with 200" do
+      get :new
+      expect(response).to have_http_status(200)
+    end
+  end
 
+  describe "USER /" do
+    it "responds with 200" do
+      post :create, params: { user: { username: "Hello, world!", password: "password"} }
+      expect(response).to redirect_to("http://test.host/posts/index")
+    end
+  end
 end
